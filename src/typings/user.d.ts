c9ae7f17dcd8141511a840fee58fb820dc7e27de
mark bashir <beejayphil@gmail.com>
2019-08-31 11:37:08 +0100

    typings for user model

---
+++ src/typings/user.d.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+interface Iuser extends mongoose.Document {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  gender: string;
+  isAdmin: boolean;
+  createdAt: Date;
+  updatedAt: Date | null;
+}
+export default Iuser;
