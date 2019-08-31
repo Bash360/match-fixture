e9880fc56294c55d925f6b110c9bc3401d064167
mark bashir <beejayphil@gmail.com>
2019-08-31 14:31:19 +0100

    removed console statement

--- __tests__/user.ts
+++ __tests__/user.ts
@@ -2,29 +2,29 @@ import { createUser } from '../src/controllers/user';
 import { connectToDB, disconnectFromDB } from '../test-setup/test-connection';
 
 describe('test for user controller', () => {
-  beforeAll(async () => { 
+  beforeAll(async () => {
     await connectToDB();
-  })
-  afterAll(async () => { 
+  });
+  afterAll(async () => {
     await disconnectFromDB();
   });
-it('should create user and return user details', async () => {
-  const result = await createUser("mark", "bashir", "beejayphil@gmail.com", "male", "bashbash")
-  expect(result).toMatchObject(
-    {
+  it('should create user and return user details', async () => {
+    const result = await createUser(
+      'mark',
+      'bashir',
+      'beejayphil@gmail.com',
+      'male',
+      'bashbash',
+    );
+    expect(result).toMatchObject({
       isAdmin: expect.any(Boolean),
       id: expect.any(String),
-      firstName:expect.any(String),
-      lastName:expect.any(String) ,
+      firstName: expect.any(String),
+      lastName: expect.any(String),
       email: expect.any(String),
-      gender:  expect.any(String),
+      gender: expect.any(String),
       createdAt: expect.any(Date),
-    updatedAt: expect.any(Date)
-});
-
-  console.log(result);
-});
-
-
-
+      updatedAt: expect.any(Date),
+    });
+  });
 });
