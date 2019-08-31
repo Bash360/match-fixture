5b9c9f05c5ebbf8251afe850968aa4af2fb78196
mark bashir <beejayphil@gmail.com>
2019-08-31 13:47:55 +0100

    fixed bug in enum type for gender field in user schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,7 +7,7 @@ const UserSchema = new Schema(
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    gender: { enum: ['male', 'female'], reqired: true },
+    gender: { type: String, enum: ['male', 'female'], required: true },
     isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true },
