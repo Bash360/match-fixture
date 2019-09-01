748c3a17f89519abcc8cebbfd594c1fbf5028f43
mark bashir <beejayphil@gmail.com>
2019-09-01 17:01:44 +0100

    typings for team

--- src/typings/team.d.ts
+++ src/typings/team.d.ts
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
 interface Iteam extends mongoose.Document {
-  name: string;
+  id: String;
+  name: String;
+  code: String;
+  logo: String;
+  country: String;
+  founded: Date;
+  stadiumName: String;
+  stadiumAddress: String;
+  city: String;
+  stadiumCapacity: Number;
 }
 export default Iteam;
