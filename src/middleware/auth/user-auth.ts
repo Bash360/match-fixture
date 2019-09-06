807c24f1af30fbc4d6ce393f533d361b2b7d1196
mark bashir <beejayphil@gmail.com>
2019-09-06 11:08:21 +0100

    added rate limiting function

--- src/middleware/auth/user-auth.ts
+++ src/middleware/auth/user-auth.ts
@@ -11,20 +11,23 @@ if (process.env.SECRET) {
 function userAuth(req: any, res: express.Response, next: express.NextFunction) {
   const token: any = req.header('authorization');
   if (!token) return res.status(401).json('Access denied no token provided');
-  const limit = req.session.hasOwnProperty('limit') ? req.session.limit : null;
-  if (limit >= 5)
-    try {
-      let bearerToken = token.split(' ')[1];
-      const decoded: any = jwt.verify(bearerToken, secret);
+  try {
+    let bearerToken = token.split(' ')[1];
+    const decoded: any = jwt.verify(bearerToken, secret);
 
-      if (decoded) {
-        res.locals.details = decoded;
-        return next();
-      } else {
-        return res.status(403).json('only admin allowed');
-      }
-    } catch (error) {
-      return res.status(400).json('invalid token');
+    if (decoded) {
+      res.locals.details = decoded;
+      return next();
+    } else {
+      return res.status(403).json('only admin allowed');
     }
+  } catch (error) {
+    return res.status(400).json('invalid token');
+  }
 }
-export default userAuth;
+function limitAPI(req: any, res: express.Response, next: express.NextFunction) {
+  const limit = req.session.hasOwnProperty('limit') ? req.session.limit : null;
+  if (limit >= 5) return res.status(429).json({ message: 'Too Many Requests' });
+  return next();
+}
+export { userAuth, limitAPI };
