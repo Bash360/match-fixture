1b47e1729441cf6edaf5db8c6747cfb3457c7f41
mark bashir <beejayphil@gmail.com>
2019-09-08 10:09:07 +0100

    removed fixtures and folder not using it

--- src/seed/teams/index.ts
+++ src/seed/teams/index.ts
@@ -8,11 +8,7 @@ const teams = [
     founded: '10-07-1899',
     headCoach: 'E. Howe',
     stadiumName: 'Vitality Stadium',
-    stadiumAddress: `
-    Dean Court, Kings Park
-BH7 7AF
-Bournemouth, Dorset
-    `,
+    stadiumAddress: `Dean Court, Kings Park BH7 7AF Bournemouth, Dorset`,
     stadiumCapacity: 12000,
   },
   {
@@ -24,11 +20,7 @@ Bournemouth, Dorset
     founded: '10-07-1880',
     headCoach: 'Guardiola',
     stadiumName: 'Etihad Stadium',
-    stadiumAddress: `
-   Rowsley Street
-M11 3FF
-Manchester
-    `,
+    stadiumAddress: `Rowsley Street M11 3FF Manchester`,
     stadiumCapacity: 55097,
   },
   {
@@ -40,11 +32,7 @@ Manchester
     founded: '10-07-1892',
     headCoach: 'S. Bruce',
     stadiumName: 'St. James Park',
-    stadiumAddress: `
-  St. James' Park
-NE1 4ST
-Newcastle upon Tyne
-    `,
+    stadiumAddress: `St. James' Park NE1 4ST Newcastle upon Tyne`,
     stadiumCapacity: 52389,
   },
   {
@@ -56,11 +44,7 @@ Newcastle upon Tyne
     founded: '10-07-1889',
     headCoach: 'C. Wilder',
     stadiumName: 'Bramall Lane',
-    stadiumAddress: `
-  Bramall Lane
-S2 4SU
-Sheffield
-    `,
+    stadiumAddress: `Bramall Lane S2 4SU Sheffield`,
     stadiumCapacity: 32702,
   },
   {
@@ -72,11 +56,7 @@ Sheffield
     founded: '10-07-1882',
     headCoach: 'M. Pochettino',
     stadiumName: 'Tottenham Hotspur Stadium',
-    stadiumAddress: `
- Bill Nicholson Way, 748 High Road
-N17 OAP
-London
-    `,
+    stadiumAddress: `Bill Nicholson Way, 748 High Road N17 OAP London`,
     stadiumCapacity: 62062,
   },
   {
@@ -88,11 +68,7 @@ London
     founded: '10-07-1895',
     headCoach: 'M. Pellegrini',
     stadiumName: 'London Stadium',
-    stadiumAddress: `
-Green Street
-E13 9AZ
-London
-    `,
+    stadiumAddress: `Green Street E13 9AZ London`,
     stadiumCapacity: 60000,
   },
   {
@@ -104,11 +80,7 @@ London
     founded: '10-07-1874',
     headCoach: 'D. Smith',
     stadiumName: 'Villa Park',
-    stadiumAddress: `
-Trinity Road
-B6 6HE
-Birmingham
-    `,
+    stadiumAddress: `Trinity Road B6 6HE Birmingham`,
     stadiumCapacity: 42788,
   },
   {
@@ -120,11 +92,7 @@ Birmingham
     founded: '10-07-1882',
     headCoach: 'S. Dyche',
     stadiumName: 'Turf Moo',
-    stadiumAddress: `
-Harry Potts Way
-BB10 4BX
-Burnley, Lancashire
-    `,
+    stadiumAddress: `Harry Potts Way BB10 4BX Burnley, Lancashire`,
     stadiumCapacity: 22546,
   },
   {
@@ -136,11 +104,7 @@ Burnley, Lancashire
     founded: '10-07-1905',
     headCoach: 'R. Hodgson',
     stadiumName: 'Selhurst Park',
-    stadiumAddress: `
-Selhurst Park
-SE25 6PU
-London
-    `,
+    stadiumAddress: `Selhurst Park SE25 6PU London`,
     stadiumCapacity: 26309,
   },
   {
@@ -152,11 +116,7 @@ London
     founded: '10-07-1884',
     headCoach: 'B. Rodgers',
     stadiumName: 'King Power Stadium',
-    stadiumAddress: `
-Filbert Way
-LE2 7FL
-Leicester
-    `,
+    stadiumAddress: `Filbert Way LE2 7FL Leicester`,
     stadiumCapacity: 32262,
   },
   {
@@ -168,11 +128,7 @@ Leicester
     founded: '10-07-1886',
     headCoach: 'B. Rodgers',
     stadiumName: 'Emirates Stadium',
-    stadiumAddress: `
-Highbury House, 75 Drayton Park
-N5 1BU
-London
-    `,
+    stadiumAddress: `Highbury House, 75 Drayton Park N5 1BU London`,
     stadiumCapacity: 60355,
   },
   {
@@ -184,11 +140,7 @@ London
     founded: '10-07-1886',
     headCoach: 'O. Solskjær',
     stadiumName: 'Old Trafford',
-    stadiumAddress: `
-Sir Matt Busby Way
-M16 0RA
-Manchester
-    `,
+    stadiumAddress: `Sir Matt Busby Way M16 0RA Manchester`,
     stadiumCapacity: 76212,
   },
   {
@@ -200,11 +152,7 @@ Manchester
     founded: '10-07-1886',
     headCoach: 'D. Farke',
     stadiumName: 'Carrow Road',
-    stadiumAddress: `
-Carrow Road
-NR1 1JE
-Norwich, Norfolk
-    `,
+    stadiumAddress: `Carrow Road NR1 1JE Norwich, Norfolk`,
     stadiumCapacity: 27244,
   },
   {
@@ -216,11 +164,7 @@ Norwich, Norfolk
     founded: '10-07-1885',
     headCoach: 'R. Hasenhüttl',
     stadiumName: 'St. Marys Stadium',
-    stadiumAddress: `
-Britannia Road
-S014 5FP
-Southampton, Hampshire
-    `,
+    stadiumAddress: `Britannia Road S014 5FP Southampton, Hampshire `,
     stadiumCapacity: 32689,
   },
   {
@@ -232,11 +176,7 @@ Southampton, Hampshire
     founded: '10-07-1881',
     headCoach: 'Javi Gracia',
     stadiumName: 'Vicarage Road',
-    stadiumAddress: `
-Vicarage Road
-WD18 0ER
-Watford
-    `,
+    stadiumAddress: `Vicarage Road WD18 0ER Watford`,
     stadiumCapacity: 21577,
   },
   {
@@ -248,10 +188,7 @@ Watford
     founded: '10-07-1877',
     headCoach: 'Nuno Espírito Santo',
     stadiumName: 'Molineux Stadium',
-    stadiumAddress: `
-Waterloo Road
-WV1 4QR
-Wolverhampton
+    stadiumAddress: `Waterloo Road WV1 4QR Wolverhampton
     `,
     stadiumCapacity: 31700,
   },
@@ -264,9 +201,7 @@ Wolverhampton
     founded: '10-07-1905',
     headCoach: 'Roy Hodgson',
     stadiumName: 'Selhurst Park',
-    stadiumAddress: `
-Hyde Park, London
-    `,
+    stadiumAddress: `Hyde Park, London`,
     stadiumCapacity: 25486,
   },
   {
@@ -278,11 +213,7 @@ Hyde Park, London
     founded: '10-07-1877',
     headCoach: 'G. Potter',
     stadiumName: 'The American Express Community Stadium',
-    stadiumAddress: `
-Village Way
-BN1 9BL
-Brighton, East Sussex
-    `,
+    stadiumAddress: `Village Way BN1 9BL Brighton, East Sussex`,
     stadiumCapacity: 30750,
   },
   {
@@ -294,11 +225,7 @@ Brighton, East Sussex
     founded: '10-07-1877',
     headCoach: 'F. Lampard',
     stadiumName: 'Stamford Bridge',
-    stadiumAddress: `
-Fulham Road
-SW6 1HS
-London
-    `,
+    stadiumAddress: `Fulham Road SW6 1HS London`,
     stadiumCapacity: 41841,
   },
   {
@@ -310,10 +237,7 @@ London
     founded: '10-07-1877',
     headCoach: 'Marco Silva',
     stadiumName: 'Goodison Park',
-    stadiumAddress: `
-Goodison Road
-L4 4EL
-Liverpool
+    stadiumAddress: `Goodison RoadL4 4EL Liverpool
     `,
     stadiumCapacity: 40569,
   },
@@ -326,11 +250,7 @@ Liverpool
     founded: '10-07-1892',
     headCoach: 'Marco Silva',
     stadiumName: 'Anfield',
-    stadiumAddress: `
-Anfield Road
-L4 0TH
-Liverpool
-    `,
+    stadiumAddress: `Anfield RoadL4 0TH Liverpool`,
     stadiumCapacity: 55212,
   },
   {
@@ -340,11 +260,9 @@ Liverpool
     country: 'England',
     city: ' Blackburn,',
     founded: '10-07-1875',
-    headCoach: '	Tony Mowbray',
-    stadiumName: '	Ewood Park, Blackburn',
-    stadiumAddress: `
-Ewood Park, Blackbur
-    `,
+    headCoach: 'Tony Mowbray',
+    stadiumName: 'Ewood Park, Blackburn',
+    stadiumAddress: `Ewood Park, Blackbur`,
     stadiumCapacity: 55212,
   },
 ];
